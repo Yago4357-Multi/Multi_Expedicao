c99bf29e1eaed1149d19c5caa54b2a85429508a4
Yago <yagolimabortolini@gmail.com>
2024-04-05 13:48:12 -0300

    Alterando Web para Windows

--- android/app/src/main/kotlin/com/example/romaneio_teste/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.romaneio_teste
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 import '/Controls/Visualiza%C3%A7%C3%A3o.dart';
 import 'Rotas.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MyApp());
 }
 
